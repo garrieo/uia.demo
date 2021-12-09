48475f3891257e41a78d215fb554c80cf6728831
Garrie Oghidi <garrieoghidi@microsoft.com>
2021-12-09 11:12:37 +0100

    snapLyaout Support impl

--- Program.cs
+++ Program.cs
@@ -17,8 +17,13 @@ namespace uia_test_console
         static void Main(string[] args)
         {
 
+
+            
+            var snapResult = SnapLayoutAppSizeSupport.launchApplication(@"C:\Windows\System32\notepad.exe");
+
             var process = Process.Start("notepad.exe");
-            int ct = 0;
+            var id = process.Id;
+                int ct = 0;
             AutomationElement msPaintAutomationElement = null;
             while (process.MainWindowHandle == IntPtr.Zero)
             {
@@ -29,17 +34,34 @@ namespace uia_test_console
             msPaintAutomationElement = AutomationElement.FromHandle(process.MainWindowHandle);
             if (msPaintAutomationElement != null)
             {
-
+                
                 var pattern = (WindowPattern)msPaintAutomationElement.GetCurrentPattern(WindowPattern.Pattern);
-                pattern.SetWindowVisualState(WindowVisualState.Maximized);
+              //  pattern.SetWindowVisualState(WindowVisualState.Maximized);
 
                 var rectMain = (System.Windows.Rect)msPaintAutomationElement.GetCurrentPropertyValue(AutomationElement.BoundingRectangleProperty);
                 var titleBar =  msPaintAutomationElement.FindFirst(TreeScope.Children, new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.TitleBar));
                 var rectTitleBar = (System.Windows.Rect)titleBar.GetCurrentPropertyValue(AutomationElement.BoundingRectangleProperty);
-
                 
-                double height = rectTitleBar.Height;
+                
+                var buttonList = titleBar.FindAll(TreeScope.Children, new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.Button));
 
+                var maxButton = buttonList.Cast<AutomationElement>().ToList().FirstOrDefault(x => x.Current.Name == "Maximize");
+
+                var cursor = new Cursor(Cursor.Current.Handle);
+                var maxButtonRectangle = (System.Windows.Rect)maxButton.GetCurrentPropertyValue(AutomationElement.BoundingRectangleProperty);
+
+
+                Cursor.Position = new System.Drawing.Point((int)maxButtonRectangle.X + 2, (int)maxButtonRectangle.Y + 2);
+
+                var comboCacheRequest = new CacheRequest();
+                comboCacheRequest.Add(SelectionPattern.Pattern);
+                comboCacheRequest.Add(SelectionPattern.SelectionProperty);
+                comboCacheRequest.Add(AutomationElement.NameProperty);
+                comboCacheRequest.TreeScope = TreeScope.Element | TreeScope.Descendants;
+
+                msPaintAutomationElement = msPaintAutomationElement.GetUpdatedCache(comboCacheRequest);
+               var panels = msPaintAutomationElement.FindAll(TreeScope.Children, new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.Pane));
+                double height = rectTitleBar.Height;
                 double h = rectTitleBar.Bottom - rectTitleBar.Top;
                 var titleBarName = titleBar.Current.Name;
                 // titleBar.Current.n
---
+++ SnapLayoutAppSizeSupport.cs
@@ -0,0 +1,89 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Remote;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uia_test_console
+{
+    public class SnapLayoutAppSizeSupport
+    {
+        public static int launchApplication(string path)
+        {
+            try
+            {
+                AppiumOptions options = new AppiumOptions();
+                options.AddAdditionalCapability("app", @"C:\Windows\System32\notepad.exe");
+
+                var notePadSession = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), options);
+
+
+                //notePadSession.Manage().Window.Maximize();
+                //var maxWindowAction = new Actions(notePadSession);
+                //maxWindowAction.SendKeys(Keys.Command + Keys.ArrowUp + Keys.Command).Build().Perform();
+
+
+                var notePadSource = notePadSession.PageSource;
+
+
+                var maxButton = notePadSession.FindElementByName("Maximize");
+                //var maxButton = notePadSession.FindElementByName("Restore");
+
+
+                var action = new Actions(notePadSession);
+                action.MoveToElement(maxButton).MoveByOffset(5, 5).Build().Perform();
+
+                //System.Threading.Thread.Sleep(1000);
+
+                //notePadSession.SwitchTo().Window(notePadSession.WindowHandles.First());
+                var deskTopOptions = new AppiumOptions();
+                deskTopOptions.AddAdditionalCapability("app", "Root");
+                var deskTopSession = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), deskTopOptions);
+
+                var popHostElement = deskTopSession.FindElementByName("PopupHost");
+                var popUpElement = popHostElement.FindElementByName("Popup");
+                var snapLayOutElement = popUpElement.FindElementByName("Snap Layouts Menu");
+
+                var windowSize = notePadSession.Manage().Window.Size;
+                /*   
+                 *   notePadSession.Manage().Window.Position
+                 *   notePadSession.Manage().Window.Maximize()
+                 *   notePadSession.Manage().Window.Size
+                 *   snapLayOutElement.Size;
+                   snapLayOutElement.Rect;
+                   snapLayOutElement.Location; 
+
+                 */
+
+                var snapLayoutAction = new Actions(deskTopSession);
+
+                var twoWindowSplitGroup = snapLayOutElement.FindElementByName("2 window split layout");
+                var twoWindowSplitGroupButton = snapLayOutElement.FindElementByName("Left half, 50% width, 100% height");
+                snapLayoutAction.Click(twoWindowSplitGroupButton).Build().Perform();
+
+                var twoWindowLeftLayoutGroup = snapLayOutElement.FindElementByName("2 window focus on left layout");
+
+
+                snapLayoutAction.MoveToElement(twoWindowLeftLayoutGroup).Build().Perform();
+
+
+                snapLayoutAction.MoveToElement(twoWindowSplitGroup).Build().Perform();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message + "\n" + e.StackTrace);
+            }
+         
+            return 0;
+        }
+
+
+    }
+}
